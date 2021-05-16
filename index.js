c12e561bbd2d88a1efca51f398675c3130368458
stacy <stacydn23@gmail.com>
2021-05-16 17:19:54 -0400

    commit tree 0a23fdf7adc865c0bb0b5f75cb4b92b3c2856206

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let theText = document.getElementById('text');
+    theText.innerHTML = "This is really cool!"
+});
